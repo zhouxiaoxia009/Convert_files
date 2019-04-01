a6eac02d84c1c5c503c50c7d4e5cd1a3da8c1fd5
zhouxiaoxia009 <40835526+zhouxiaoxia009@users.noreply.github.com>
2019-04-01 15:45:07 +0800

    Add files via upload
    to read csv files in DAX2

---
+++ readdax2.py
@@ -0,0 +1,15 @@
+﻿import os
+import pandas as pd
+
+
+path = "E:\\Xiaoxia\\py\\dax2"
+
+
+# get filename under folder
+dirs = os.listdir(path)
+for filename in dirs:
+    # filter csv files
+    if os.path.splitext(filename)[1] == ".csv":
+        print(filename)
+        file = pd.read_csv(path + "\\" + filename, header=0, sep='\t*\;',engine='python')
+        file.to_csv(filename, index=False)
